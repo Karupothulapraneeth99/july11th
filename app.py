69ecb1c288233f2ff89ca641c55343ca1536406c
praneeth <praneethchanti22@hotmail.com>
2025-07-11 23:28:12 +0530

    added app.py and updated code

---
+++ app.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        print(f"Student name is {self.name}")
+
+if __name__ == "__main__":
+    chanti = Student("Chanti")
